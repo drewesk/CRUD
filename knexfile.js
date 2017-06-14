471b10099349edc5213b0b503759ea1f0ce4da80
Andrew Eskenazi <drewesk@gmail.com>
2017-06-13 18:35:47 -0600

    Adding production db connections.

--- knexfile.js
+++ knexfile.js
@@ -10,4 +10,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/test-events'
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
